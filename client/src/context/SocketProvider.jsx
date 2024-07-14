053b57dd38b6441aee1551576beb70a53c3fd75f
ANUSHKA BHANDARI <99523033+Anushkabh@users.noreply.github.com>
2024-07-15 00:31:07 +0530

    Update SocketProvider.jsx

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("https://drishti-nine.vercel.app/"), []);
+  const socket = useMemo(() => io("https://drishti-bg3z.vercel.app/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
